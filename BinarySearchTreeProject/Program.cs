626ea15d5f1885efd3b5f294f4f8609e589d222b
Faiz <faiz.anwar4749@gmail.com>
2020-11-16 13:06:37 +0530

    [Faiz] Add: Displaying Welcome message

---
+++ BinarySearchTreeProject/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BinarySearchTreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program.");
+        }
+    }
+}
