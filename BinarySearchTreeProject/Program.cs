81d56c4a6cc676c111637ef6fefc64eed734ef6d
Faiz <faiz.anwar4749@gmail.com>
2020-11-16 13:11:20 +0530

    [Faiz] Add: Method to add nodes to BST

--- BinarySearchTreeProject/Program.cs
+++ BinarySearchTreeProject/Program.cs
@@ -6,6 +6,10 @@ namespace BinarySearchTreeProject
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Program.");
+            BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
         }
     }
 }
